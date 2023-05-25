975035289eec1f032513b720517fd4b6f8b8eced
Amit Das <amitdas@Amits-MacBook-Pro.local>
2023-05-25 18:21:47 -0400

    cleaned up

--- transform.py
+++ transform.py
@@ -34,3 +34,22 @@ class Transform:
         x, y = point
         transformed_point = np.matmul(transformation_matrix, np.array([x, y, 0, 1], dtype='float64'))
         return (transformed_point[0], transformed_point[1])
+    def _get_rotation(self, target, source):
+        """
+        Gets the angle in the pose of the robot in the target
+        w.r.t the source reference frame
+        """
+        has_found_transformation = False
+        while not has_found_transformation:
+            try:
+                trans, rot = self.transform_listener.lookupTransform(
+                    source,
+                    target,
+                    rospy.Time(0)
+                )
+                has_found_transformation = True
+            except tf.LookupException:
+                rospy.sleep(0.5)
+                continue
+        yaw = tf.transformations.euler_from_quaternion(rot)[2]
+        return yaw
