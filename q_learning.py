1b7092b1870154c8c88ba0e22b10971bf9175f39
Amit Das <amitdas@Amits-MacBook-Pro.local>
2023-06-02 19:24:51 -0400

    final version

--- q_learning.py
+++ q_learning.py
@@ -6,9 +6,9 @@ import math
 import random
 import matplotlib.pyplot as plt
 
-DECAY_RATE = 0.5
-MAX_ENERGY = 20
-ERRORS = [] # keep track of errors after each training iteration
+DECAY_RATE = 0.95 # epsilon-greedy
+MAX_ENERGY = 20 # max energy state
+Q_CHANGES = [] # changes from training iterations
 
 class QLearning:
     """
@@ -19,23 +19,18 @@ class QLearning:
         height(int)
         start_loc (tuple): (x, y)
         target_loc(tuple): (x, y)
-    
-    Attributes:
-        alpha (float): learning rate
-        gamma (float): discount factor
-        q_table (dict): Q-table
-        best_policy (dict): best action to take for each state
     """
 
     def __init__(self, width, height, start_loc, target_loc):
+        """Initialization."""
         self.target = target_loc
-        self.alpha = 0.5
-        self.gamma = 0.1
+        self.alpha = 0.5 # learning-rate
+        self.gamma = 0.2 # exploration 
         self.q_table = {}
         self.best_policy = {}
         self.height = height
         self.width = width
-        self.epsilon = 1
+        self.epsilon = 1.0 # epsilon-greedy
         self.is_close_to_robot = False
         self.in_energy_zone = False
         
@@ -54,7 +49,7 @@ class QLearning:
         Mainly called by the training function
 
         Args:
-            state_action_pair (tuple): (x, y, action)
+            state_action_pair (tuple): (x, y, energy, action)
         """
         
         state_x = state_action_pair[0]
@@ -62,6 +57,7 @@ class QLearning:
         state_energy = state_action_pair[2]
         action = state_action_pair[3]
 
+        # computing new state based on possible actions
         if action == 'up':
             new_state_x = state_x
             new_state_y = min(self.height - 1, state_y + 1)
@@ -80,6 +76,7 @@ class QLearning:
         else:
             new_energy = max(0, state_energy - 1)
         
+        # calculating max over the next possible states
         next_state_max_val = max(self.q_table[(new_state_x, new_state_y, new_energy, 'right')],
                                  self.q_table[(new_state_x, new_state_y, new_energy, 'left')],
                                  self.q_table[(new_state_x, new_state_y, new_energy, 'up')],
@@ -89,6 +86,7 @@ class QLearning:
         q_value = (1-self.alpha)*self.q_table[state_action_pair] + self.alpha*(
             self.calculate_reward((new_state_x, new_state_y), new_energy) + self.gamma*next_state_max_val)
         
+        # updating the q-table with new value
         self.q_table[state_action_pair] = q_value
         
     def calculate_reward(self, point, curr_energy):
@@ -105,17 +103,17 @@ class QLearning:
 
         max_reward = 1000 # reward for closely following object
 
-        # robot too close to object
+        # penalty if robot is too close to moving object
         proximity_penalty = 0
         if self.is_close_to_robot:
             proximity_penalty = -100
 
         distance_to_object = math.sqrt((self.target[0] - point[0])**2 + (self.target[1] - point[1])**2)
-        following_reward = max_reward/(1 + distance_to_object)
+        following_reward = max_reward/(1 + distance_to_object) # reward to encourage following
         
-        low_energy_penalty = -2500 * math.sqrt((1 - (float(curr_energy) / 20)))
+        low_energy_penalty = -2250 * math.sqrt((1 - (float(curr_energy) / 20))) # change to -100 if you want to prioritize object following
 
-        edge_penalty = 0 # too close to edge of grid
+        edge_penalty = 0 # penalty given if robot is too close to edges of environment
         if point[0] >= 9 or point[1] >= 9 or point[0] <= 1 or point[1] <= 1:
             edge_penalty = -1000
 
@@ -128,19 +126,20 @@ class QLearning:
         Args:
             iterations (int): number of iterations to train
         """
-        
+
         for i in range(iterations):
-            prev_total = sum(self.q_table.values())
+            prev_total = sum(self.q_table.values()) # sum of table before iteration
             for key in self.q_table.keys():
                 self.update_q_table(key)
             self.get_best_policy(self.q_table)
-            new_total = sum(self.q_table.values())
+            new_total = sum(self.q_table.values()) # sum of table after iteration
 
-            ERRORS.append(abs(new_total - prev_total))
+            Q_CHANGES.append(abs(new_total - prev_total))
+            
+            # checks for convergence; stops if training converges
             if abs(new_total - prev_total) < 100:
-                print((new_total), (prev_total))
                 print("Converges during Iteration " + str(i))
-                break # stops training after convergence
+                break
 
     
     def get_best_policy(self, q_table):
@@ -155,8 +154,7 @@ class QLearning:
         for key in q_table.keys():
             all_states.add((key[0], key[1], key[2]))
 
-        self.epsilon = self.epsilon * DECAY_RATE
-        
+        self.epsilon = self.epsilon * DECAY_RATE # epsilon-greedy
         
         for state in all_states:
             actions = {
@@ -176,21 +174,22 @@ class QLearning:
                 self.best_policy[(state[0], state[1], state[2])] = pos_actions[random_index]
 
 if __name__ == "__main__":
+    
+    # Q-learning
     q_learning = QLearning(20, 20, (0, 0), (5, 5))
     q_learning.training(200)
     q_learning.get_best_policy(q_learning.q_table)
 
-    # Plotting the error over iterations graph
-    plt.plot([i for i in range(len(ERRORS))], ERRORS, color='green', linewidth=2, marker='o', markersize=4)
+    # Plotting changes of Q-values from iteration to iteration
+    plt.plot([i for i in range(len(Q_CHANGES))], Q_CHANGES, color='green', linewidth=2, marker='o', markersize=4)
 
-    # Setting up the grid
+    # Setting up the plot grid
     plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
-    plt.ylabel("Error")
-    plt.xlabel("Iterations")
-    plt.title("Error over Iterations")
+    plt.ylabel("Net Difference between Consecutive Iterations")
+    plt.xlabel("Training Iterations")
+    plt.title("Deviation of Q-values over Training Iterations")
 
-    # Setting up the axes
-    plt.axhline(y=0, color='black', linewidth=1)
-    plt.axvline(x=0, color='black', linewidth=1)
+    # Setting up the plot axes
+    plt.axhline(y=0, linewidth = 1)
+    plt.axvline(x=0, linewidth = 1)
     plt.show()
-
